c2c93fc5a50d9b9d49a120ac8bea5eb7c3e33f19
rupeshrrts <rupeshsinghsuntech@gmail.com>
2026-03-07 18:40:27 +0530

    generalized code

--- Coding_Problems/problem4.cpp
+++ Coding_Problems/problem4.cpp
@@ -2,35 +2,122 @@
 // o/p=abcbd
 // i/p =""
 // o/p =
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+// void replace(char input[])
+// {
+//     int l = strlen(input);
+//     if (l == 0)
+//     {
+//         return;
+//     }
+//     if (input[0] == 'x')
+//     {
+//         for (int i = 0; i < l; i++)
+//         {
+//             input[i] = input[i + 1];
+//         }
+//         replace(input);
+//     }
+//     else // if first chsr is not 'x'
+//     {
+//         replace(input + 1);
+//     }
+// }
+
+// int main()
+// {
+//     char input[100];
+//     cout << "Enter the char: " << endl;
+//     cin >> input;
+//     replace(input);
+//     cout << input << endl;
+// }
+
+// // replace every occrrence with x--> aam
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+// void replace(char input[])
+// {
+//     int l = strlen(input);
+//     if (l == 0)
+//     {
+//         return;
+//     }
+//     if (input[0] == 'x')
+//     {
+//         for (int i = l; i > 0; i--)
+//         {
+//             input[i + 2] = input[i];
+//         }
+//         input[0] = 'r';
+//         input[1] = 'a';
+//         input[2] = 'm';
+//         replace(input + 3);
+//     }
+//     else // if first chsr is not 'x'
+//     {
+//         replace(input + 1);
+//     }
+// }
+
+// int main()
+// {
+//     char input[100];
+//     cout << "Enter the char: " << endl;
+//     cin >> input;
+//     replace(input);
+//     cout << input << endl;
+// }
+
+// generalized code
+//  replace every occrrence with x--> aam
 #include <iostream>
 #include <cstring>
 using namespace std;
-void replace(char input[])
+
+void replace(char input[], char s, const char str[])
 {
-    int l = strlen(input);
-    if (l == 0)
+    int l1 = strlen(input);
+    int l2 = strlen(str);
+
+    if (l1 == 0)
     {
         return;
     }
-    if (input[0] == 'x')
+
+    if (input[0] == s)
     {
-        for (int i = 0; i < l; i++)
+        // shift characters
+        for (int i = l1; i >= 0; i--)
         {
-            input[i] = input[i + 1];
+            input[i + l2 - 1] = input[i];
         }
-        replace(input);
+
+        // insert replacement string
+        for (int i = 0; i < l2; i++)
+        {
+            input[i] = str[i];
+        }
+
+        replace(input + l2, s, str);
     }
-    else // if first chsr is not 'x'
+    else
     {
-        replace(input + 1);
+        replace(input + 1, s, str);
     }
 }
 
 int main()
 {
     char input[100];
-    cout << "Enter the char: " << endl;
+
+    cout << "Enter the string: ";
     cin >> input;
-    replace(input);
+
+    replace(input, 'x', "ram");
+
     cout << input << endl;
 }
