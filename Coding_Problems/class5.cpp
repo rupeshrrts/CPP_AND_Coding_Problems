7b8dbd8d40a400b0402ac4a559f8430e79589ac1
rupeshrrts <rupeshsinghsuntech@gmail.com>
2026-03-09 21:37:05 +0530

    Undate code 5

--- Coding_Problems/class5.cpp
+++ Coding_Problems/class5.cpp
@@ -46,3 +46,33 @@ int main()
         cout << "}" << endl;
     }
 }
+
+// // 2. write all the permutation of the given string
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// void permute(int index, string input, vector<string> &ans)
+// {
+//     if (index == input.size())
+//     {
+//         ans.push_back(input);
+//         return;
+//     }
+//     for (int j = index; j < input.size(); j++)
+//     {
+//         swap(input[index], input[j]);
+//         permute(index + 1, input, ans);
+//         swap(input[index], input[j]);
+//     }
+// }
+// int main()
+// {
+//     string input;
+//     cin >> input;
+//     vector<string> ans;
+//     permute(0, input, ans);
+//     for (auto elem : ans)
+//     {
+//         cout << elem << endl;
+//     }
+// }
