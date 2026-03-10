93b67f926f3cc083463f3e1e8c7ec9e5819d86e0
rupeshrrts <rupeshsinghsuntech@gmail.com>
2026-03-10 21:46:30 +0530

    update

--- Coding_Problems/problem1.cpp
+++ Coding_Problems/problem1.cpp
@@ -1,5 +1,22 @@
 // Sum of all element of an array
 
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[6] = {4, 7, 2, 4, 5, 7};
+//     int sum = 0;
+//     for (int i = 0; i < 6; i++)
+//     {
+//         sum += arr[i];
+//     }
+//     cout << "Sum is: " << sum << endl;
+// }
+
+// Sum of all element of an array
+
 #include <iostream>
 using namespace std;
 int findSum(int arr[], int size)
--- Coding_Problems/problem2.cpp
+++ Coding_Problems/problem2.cpp
@@ -1,4 +1,32 @@
 // Check given array is sorted or not
+// #include <iostream>
+// using namespace std;
+// bool isSorted(int arr[], int size)
+// {
+//     for (int i = 0; i < size - 1; i++)
+//     {
+//         if (arr[i] > arr[i + 1])
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// int main()
+// {
+//     int arr[6] = {1, 3};
+//     if (isSorted(arr, 2))
+//     {
+//         cout << "Sorted";
+//     }
+//     else
+//     {
+//         cout << "Not Sorted";
+//     }
+// }
+
+// Check given array is sorted or not
 #include <iostream>
 using namespace std;
 bool isSorted(int arr[], int size)
@@ -9,10 +37,11 @@ bool isSorted(int arr[], int size)
         return false;
     return isSorted(arr + 1, size - 1);
 }
+
 int main()
 {
-    int arr[6] = {1, 3};
-    if (isSorted(arr, 2))
+    int arr[6] = {1, 3, 9, 5, 6, 3};
+    if (isSorted(arr, 6))
     {
         cout << "Sorted";
     }
